48001bf77544cc2ba222e437e30dc27d5b8e6dbb
Cornelius Rebmann <Corni>
2025-05-19 19:17:22 +0200

    mlp enhanced

--- mnist.py
+++ mnist.py
@@ -117,6 +117,12 @@ def init_mlp_for_dataset(dataset_name, hidden_dims=[128, 64], dropout=0.2):
 
     return MLP(hidden_dims=hidden_dims, input_size=input_size, num_classes=num_classes, dropout=dropout)
 
+def init_weights_kaiming(m):
+    if isinstance(m, nn.Linear):
+        nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='relu')
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+
 class MLP(nn.Module):
     def __init__(self, hidden_dims=[128, 64], input_size=784, num_classes=10, dropout=0.2):
         super(MLP, self).__init__()
@@ -127,11 +133,14 @@ class MLP(nn.Module):
         for dim in hidden_dims:
             layers.append(nn.Linear(prev_dim, dim))
             layers.append(nn.ReLU())
-            layers.append(nn.Dropout(dropout))  # Add dropout here
+            layers.append(nn.Dropout(dropout))
             prev_dim = dim
 
         self.feature_extractor = nn.Sequential(*layers)
         self.head = nn.Linear(prev_dim, num_classes)
+
+        # Apply Kaiming Initialization
+        self.apply(init_weights_kaiming)
 
     def forward(self, x, return_embedding=False):
         x = x.view(x.size(0), -1)
